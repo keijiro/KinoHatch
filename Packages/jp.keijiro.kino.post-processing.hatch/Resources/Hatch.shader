49a1b0565f20dbfb0eb23bfec53ae9bbad107c20
Keijiro Takahashi <keijiro@gmail.com>
2020-04-14 21:40:45 +0900

    Refactoring

--- Packages/jp.keijiro.kino.post-processing.hatch/Resources/Hatch.shader
+++ Packages/jp.keijiro.kino.post-processing.hatch/Resources/Hatch.shader
@@ -2,10 +2,11 @@ Shader "Hidden/Kino/PostProcess/Hatch"
 {
     HLSLINCLUDE
 
+    // -- Post processing boilerplate code --
+
     #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
     #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
     #include "Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl"
-    #include "SimplexNoise2D.hlsl"
 
     struct Attributes
     {
@@ -30,43 +31,73 @@ Shader "Hidden/Kino/PostProcess/Hatch"
         return output;
     }
 
+    // -- End of boilerplate --
+
+    #include "SimplexNoise2D.hlsl"
+
+    float _Repeat;
+    float _Waviness;
+    float _Displacement;
+    float _Thickness;
+    float _Seed;
     float _Opacity;
 
     TEXTURE2D_X(_InputTexture);
+
+    // Remap range [a, b] -> [0, 1]
+    float Range01(float x, float a, float b)
+    {
+        return saturate((x - a) / (b - a));
+    }
+
+    float Hatching(float2 uv, float2 dir, float thresh)
+    {
+        // Potential (before adding wave)
+        float p1 = dot(uv, dir) * _Repeat;
+
+        // Wave noise sample point
+        float2 nsp = float2(
+          dot(uv, dir.xy * float2(1, -1)),
+          floor(p1) * 11.4729 / _Repeat + _Seed
+        );
+
+        // Displacement by noise
+        float disp = snoise(nsp * _Repeat * _Waviness * 2);
+        disp *= 0.2 * _Displacement / _Repeat;
+
+        // Potential (wave added)
+        float p2 = dot(uv + dir * disp, dir) * _Repeat;
+
+        // Distance from p=0.5
+        float d = abs(1 - 2 * frac(p2));
+
+        // Fill if (distance - thickness) < threshold.
+        return saturate((thresh - _Thickness + d) * 400 / _Repeat);
+    }
 
     float4 Fragment(Varyings input) : SV_Target
     {
         UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
 
         // Input sample
-        float2 pss = input.texcoord * _ScreenSize.xy;
-        float4 col = LOAD_TEXTURE2D_X(_InputTexture, pss);
-
-        float2 uv = input.texcoord.xy;
-        uv += snoise(uv * 60 + 29.2783 * floor(dot(uv, 100))) * float2(1, 1) * 0.0005;
-        float bw = abs(1 - 2 * frac(dot(uv, 100)));
-
-        bw = saturate(bw * 4 + lerp(-4.0, 1, dot(col, 1.0 / 3)));
-
-
-        uv = input.texcoord.xy;
-        uv += snoise(uv * 60 + 29.2783 * floor(dot(uv, float2(-1, 1) * 100))) * float2(-1, 1) * 0.0005;
-        float bw2 = abs(1 - 2 * frac(dot(uv, float2(-1, 1) * 100)));
-
-        bw2 = saturate(bw2 * 4 + lerp(-4.0, 3, dot(col, 1.0 / 3)));
-
-        bw = min(bw, bw2);
+        float2 uv = input.texcoord;
+        float4 src = LOAD_TEXTURE2D_X(_InputTexture, uv * _ScreenSize.xy);
 
         // Linear -> sRGB
-        col.rgb = LinearToSRGB(col.rgb);
+        src.rgb = LinearToSRGB(src.rgb);
+
+        // Hatching
+        float lm = Luminance(src.rgb);
+        float bw1 = Hatching(uv, float2(1,  1), Range01(lm, 0.4, 1));
+        float bw2 = Hatching(uv, float2(1, -1), Range01(lm, 0.1, 0.7));
 
         // Opacity
-        col.rgb = lerp(col.rgb, bw, _Opacity);
+        src.rgb = lerp(src.rgb, min(bw1, bw2), _Opacity);
 
         // sRGB -> Linear
-        col.rgb = SRGBToLinear(col.rgb);
+        src.rgb = SRGBToLinear(src.rgb);
 
-        return col;
+        return src;
     }
 
     ENDHLSL
--- Packages/jp.keijiro.kino.post-processing.hatch/Runtime/Hatch.cs
+++ Packages/jp.keijiro.kino.post-processing.hatch/Runtime/Hatch.cs
@@ -5,23 +5,62 @@ using UnityEngine.Rendering.HighDefinition;
 namespace Kino.PostProcessing
 {
     [System.Serializable, VolumeComponentMenu("Post-processing/Kino/Hatch")]
-    public sealed class Hatch : CustomPostProcessVolumeComponent, IPostProcessComponent
+    public sealed class Hatch
+      : CustomPostProcessVolumeComponent, IPostProcessComponent
     {
         #region Exposed parameters
 
-        public ClampedFloatParameter opacity = new ClampedFloatParameter(0, 0, 1);
+        public ClampedFloatParameter repeat
+          = new ClampedFloatParameter(100, 0, 200);
+
+        public ClampedFloatParameter waviness
+          = new ClampedFloatParameter(0.25f, 0, 1);
+
+        public ClampedFloatParameter displacement
+          = new ClampedFloatParameter(0.1f, 0, 1);
+
+        public ClampedFloatParameter thickness
+          = new ClampedFloatParameter(0.6f, 0, 1);
+
+        public ClampedFloatParameter opacity
+          = new ClampedFloatParameter(0, 0, 1);
 
         #endregion
 
-        #region Private variables
+        #region Private members
 
         static class IDs
         {
-            internal static readonly int InputTexture = Shader.PropertyToID("_InputTexture");
-            internal static readonly int Opacity = Shader.PropertyToID("_Opacity");
+            internal static readonly int InputTexture
+              = Shader.PropertyToID("_InputTexture");
+
+            internal static readonly int Repeat
+              = Shader.PropertyToID("_Repeat");
+
+            internal static readonly int Waviness
+              = Shader.PropertyToID("_Waviness");
+
+            internal static readonly int Displacement
+              = Shader.PropertyToID("_Displacement");
+
+            internal static readonly int Thickness
+              = Shader.PropertyToID("_Thickness");
+
+            internal static readonly int Seed
+              = Shader.PropertyToID("_Seed");
+
+            internal static readonly int Opacity
+              = Shader.PropertyToID("_Opacity");
         }
 
         Material _material;
+
+        static uint JenkinsHash(uint x)
+        {
+            x += (x << 10); x ^= (x >>  6);
+            x += (x <<  3); x ^= (x >> 11);
+            return x + (x << 15);
+        }
 
         #endregion
 
@@ -29,28 +68,33 @@ namespace Kino.PostProcessing
 
         public bool IsActive() => _material != null && opacity.value > 0;
 
-        public override CustomPostProcessInjectionPoint injectionPoint =>
-            CustomPostProcessInjectionPoint.AfterPostProcess;
+        public override CustomPostProcessInjectionPoint injectionPoint
+          => CustomPostProcessInjectionPoint.AfterPostProcess;
 
         public override void Setup()
-        {
-            _material = CoreUtils.CreateEngineMaterial("Hidden/Kino/PostProcess/Hatch");
-        }
+          => _material = CoreUtils.CreateEngineMaterial
+               ("Hidden/Kino/PostProcess/Hatch");
 
-        public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle srcRT, RTHandle destRT)
+        public override void Render
+          (CommandBuffer cmd, HDCamera camera, RTHandle srcRT, RTHandle destRT)
         {
             if (_material == null) return;
 
+            var seed = (JenkinsHash((uint)Time.frameCount) & 0xffff) * 0.001f;
+
+            _material.SetFloat(IDs.Repeat, repeat.value);
+            _material.SetFloat(IDs.Waviness, waviness.value);
+            _material.SetFloat(IDs.Displacement, displacement.value);
+            _material.SetFloat(IDs.Thickness, thickness.value / 2);
             _material.SetFloat(IDs.Opacity, opacity.value);
+            _material.SetFloat(IDs.Seed, seed);
 
             _material.SetTexture(IDs.InputTexture, srcRT);
             HDUtils.DrawFullScreen(cmd, _material, destRT);
         }
 
         public override void Cleanup()
-        {
-            CoreUtils.Destroy(_material);
-        }
+          => CoreUtils.Destroy(_material);
 
         #endregion
     }
